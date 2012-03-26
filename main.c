04f40d6467b3c5e17f3b6359b23e303cc9c84923
Wallace <wallace.almeida89@gmail.com>
2012-03-26 09:21:55 -0300

    Continuando

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int *estrada;			// Vetor Compartilhado da Estrada
 int d; 					// Distância em Km
 
 typedef struct biker{
 	int id;				// Identificador
-	int vel_pl; 		// Velocidade do Ciclista em trechos Planos em Km/h
+	int vel_plano; 		// Velocidade do Ciclista em trechos Planos em Km/h
 	int vel_sub; 		// Velocidade do Ciclista em trechos de Subida em Km/h
 	int vel_desc; 		// Velocidade do Ciclista em trechos de Descida em Km/h
 }ciclista;
@@ -16,6 +17,7 @@ int main(int argc, char* argv[]){
 	int n; 				// Largura da Pista
 	char modo_vel;		// Modo de Criação da Velocidade:  'A' - Aleatório / 'U' - Uniforme 
 	
+	int k;
 	int aux = 0;
 	char trecho;
 	
@@ -23,11 +25,26 @@ int main(int argc, char* argv[]){
 	arq_entrada = fopen(argv[1],"r");
 	fscanf(arq_entrada,"%d", &m);
 	fscanf(arq_entrada,"%d", &n);
-	fscanf(arq_entrada,"%c", &modo_vel);
+	modo_vel = 'B';
+	while (modo_vel != 'A' && modo_vel != 'U') {
+		fscanf(arq_entrada,"%c", &modo_vel);
+		printf ("%c", modo_vel);
+		if (feof (arq_entrada)) {
+			printf ("Oh oh!\n");
+			exit (-2);
+		}
+	}
 	fscanf(arq_entrada,"%d", &d);
 	
+	printf("Oi estou vivo %d %d %c %d\n",m,n,modo_vel,d);
+	
 	while(aux < d){
-		fscanf(arq_entrada,"%c", &trecho);
+		while (trecho != 'P' && trecho != 'S' && trecho != 'D') {
+			if (!fscanf(arq_entrada,"%c", &trecho)) {
+				printf("Não foi possível ler o bang!\n");
+				exit(-1);
+			}
+		}
 		switch(trecho){
 			case 'P': // Trecho Plano
 				break;
@@ -38,6 +55,9 @@ int main(int argc, char* argv[]){
 			default:
 				break;
 		}
+		fscanf(arq_entrada,"%d", &k);
+		printf("trecho: %c - k: %d\n",trecho,k);
+		aux += k;
 	}
 	// Fim da Leitura
 		
